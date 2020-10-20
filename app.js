8f9321d4900549e07b6d662bb46cca3715cf7b02
JaronArmiger <jaron.armiger@gmail.com>
2020-10-20 18:08:17 -0500

    write route for getting images from s3 bucket (which I probably won't use since I just made the bucket public instead)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const AWS = require('aws-sdk');
 
 require('./database');
 
@@ -15,6 +16,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_ACCESS_KEY,
+  region: 'us-east-2',
+});
+const s3 = new AWS.S3();
+
+app.get('/get_file/:filename', (req, res) => {
+  getImage(req.params.filename, res)
+    .then((img) => {
+      let image = "<img src='data:image/jpeg;base64," +
+        encode(img.Body) + "'" + "/>";
+      res.send(image);
+    })
+    .catch((e) => {
+      res.send(e);
+    })
+})
 app.use('/api', apiRouter);
 
 app.get('*', (req, res) => {
@@ -22,3 +41,19 @@ app.get('*', (req, res) => {
 });
 
 module.exports = app;
+
+const getImage = async (filename, res) => {
+  const getParams = {
+  	Bucket: process.env.AWS_BUCKET_NAME,
+  	Key: filename,
+  };
+
+  const data = s3.getObject(getParams).promise();
+  return data;
+}
+
+const encode = (data) => {
+  let buf = Buffer.from(data);
+  let base64 = buf.toString('base64');
+  return base64;
+}
