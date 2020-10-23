d47996548b70a65fdc37b97bc3e1b15c3f274886
JaronArmiger <jaron.armiger@gmail.com>
2020-10-23 09:32:55 -0500

    finish wirting photos action

--- client/src/actions/photos.js
+++ client/src/actions/photos.js
@@ -1,5 +1,34 @@
 import axios from 'axios';
+import { getErrors } from './errors';
 
 export const beginAddPhoto = (photo) => {
-  
-}
+  return async (dispatch) => {
+    try {
+      const formData = new FormData();
+      formData.append('photo', photo);
+      await axios.post('/photos', formData, {
+      	headers: {
+      	  'Content-Type': 'multipart/form-data',
+      	}
+      });
+    } catch (err) {
+      err.response && dispatch(getErrors(err.response.data));
+    }
+  }
+};
+
+export const startLoadPhotos = () => {
+  return async (dispatch) => {
+    try {
+      const photos = await axios.get('/photos');
+      dispatch(loadPhotos(photos.data));
+    } catch (err) {
+      err.response && dispatch(getErrors(err.response.data));
+    }
+  }
+};
+
+export const loadPhotos = (photos) => ({
+  type: 'LOAD_PHOTOS',
+  photos
+})
