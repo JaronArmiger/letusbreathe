8a02790aec3fdc8055b6e875e307c8fa626abdb4
JaronArmiger <jaron.armiger@gmail.com>
2020-10-23 09:37:17 -0500

    write AppRouter

---
+++ client/src/router/AppRouter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import UploadForm from '../components/UploadForm';
+
+const AppRouter = () => {
+  <BrowserRouter>
+    <div>
+      <div className='main-content'>
+      	<Switch>
+      	  <Route component={UploadForm} path='/' exact />
+      	</Switch>
+      </div>
+    </div>
+  </BrowserRouter>
+}
