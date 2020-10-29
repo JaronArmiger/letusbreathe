84e7e20e3e5e6f76effeeab85b606af30f2dacbb
JaronArmiger <jaron.armiger@gmail.com>
2020-10-29 18:21:32 -0500

    print NODE_ENV

--- client/src/components/Photo.js
+++ client/src/components/Photo.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import {} from 'dotenv/config';
 
 const Photo = ({ url }) => {
+  let envString;
+  console.log(process.env.NODE_ENV);
+  if (process.env.NODE_ENV === 'development') {
+  	envString = 'dev';
+  }
   return (
     <Card className='photo'>
       <Card.Img
