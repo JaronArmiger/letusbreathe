6b62bfa904369d00f1bd04de21a4bc95064da4f0
JaronArmiger <jaron.armiger@gmail.com>
2020-10-30 11:52:42 -0500

    write album model

---
+++ models/album.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  name: {type: String, required: true, unique: true, dropDups: true}
+});
+
+module.exports = mongoose.model('Album', AlbumSchema);
