51059becf77a04d5434c82ec6f27c5b080f6d50d
JaronArmiger <jaron.armiger@gmail.com>
2020-10-30 11:39:00 -0500

    restore orginal first migration and add new one that changes photo field from null to reference

--- migrations/20201030163659-change-photo-field-to-ref-in-events.js
+++ migrations/20201030163659-change-photo-field-to-ref-in-events.js
@@ -1,15 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 module.exports = {
   async up(db, client) {
     await db.collection('events').updateMany({}, {
       $set: {
-        photo: null,
+        photo: {type: Schema.Types.ObjectId, ref: 'Photo'},
       }
     })
   },
 
   async down(db, client) {
     await db.collection('events').updateMany({}, {
-      $unset: {
+      $set: {
         photo: null,
       }
     })
